f0116e9d965b7a6aaf8bd16415074e4ab73f4151
Josie Beaudoin <jtranbeau@gmail.com>
2019-12-03 15:44:01 -0500

    chore: clean up code

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -76,6 +76,7 @@ class Sidebar extends React.Component {
 
           {this.state.rooms.map(room => (
             <SideNavItem
+              key={room.id}
               onClick={() => this.joinRoom(room[0], this.state.newUser)}
             >{`${room[0]}: ${room[1]} players`}</SideNavItem>
           ))}
