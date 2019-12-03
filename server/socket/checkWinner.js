42be8056bd4961e72a8dd4ea849149464b125c4c
Pauline Kong <pkong89@gmail.com>
2019-12-03 16:56:34 -0500

    fixed typo

--- server/socket/checkWinner.js
+++ server/socket/checkWinner.js
@@ -16,15 +16,15 @@ function checkWinner(rooms, roomName, io) {
     rooms[roomName].boardstate.activeTeam === 'red' &&
     rooms[roomName].boardstate.colors.grey.length
   ) {
-    console.log('RED CHOSE THE ASSASIN CARD, BLUE WINS!')
-    win('RED CHOSE THE ASSASIN CARD, BLUE WINS!', io, roomName)
+    console.log('RED CHOSE THE ASSASSIN CARD, BLUE WINS!')
+    win('RED CHOSE THE ASSASSIN CARD, BLUE WINS!', io, roomName)
   }
   if (
     rooms[roomName].boardstate.activeTeam === 'blue' &&
     rooms[roomName].boardstate.colors.grey.length
   ) {
-    console.log('BLUE CHOSE THE ASSASIN CARD, RED WINS!')
-    win('BLUE CHOSE THE ASSASIN CARD, RED WINS!', io, roomName)
+    console.log('BLUE CHOSE THE ASSASSIN CARD, RED WINS!')
+    win('BLUE CHOSE THE ASSASSIN CARD, RED WINS!', io, roomName)
   }
 }
 
