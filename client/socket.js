7f37a6093d46ea47a16074064858950af0e64359
Josie Beaudoin <jtranbeau@gmail.com>
2019-12-04 10:48:52 -0500

    fix enpoint

--- client/socket.js
+++ client/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io(window.location.origin)
+const socket = io('https://codewords-1909-gracehopper.herokuapp.com/')
 
 socket.on('connect', () => {
   console.log('Connected!')
